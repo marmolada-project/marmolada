87ef163c1b8afe884df132e62475f75f86b51b5a
Nils Philippsen <nils@tiptoe.de>
2023-05-30 22:19:53 +0200

    Configure Coverage
    Signed-off-by: Nils Philippsen <nils@tiptoe.de>

--- tests/core/test_cli.py
+++ tests/core/test_cli.py
@@ -18,11 +18,17 @@ def _test_cli():
 
 
 class TestCLI:
-    @pytest.mark.parametrize("testcase", ("without-config", "with-config", "with-missing-config"))
+    @pytest.mark.parametrize(
+        "testcase", ("without-config", "with-default-config", "with-config", "with-missing-config")
+    )
     @mock.patch("marmolada.core.configuration.read_configuration")
     def test_init_config(self, read_configuration, testcase, tmp_path, cli_runner):
         args = ("test",)
         match testcase:
+            case "without-config":
+                read_configuration.side_effect = FileNotFoundError
+            case "with-default-config":
+                pass
             case "with-config":
                 args = ("-c", "/dev/null", *args)
             case "with-missing-config":
@@ -32,7 +38,7 @@ class TestCLI:
         result = cli_runner.invoke(cli.cli, args)
 
         match testcase:
-            case "without-config":
+            case "without-config" | "with-default-config":
                 assert result.exit_code == 0
                 read_configuration.assert_called_once_with(
                     cli.DEFAULT_CONFIG_FILE, clear=True, validate=True
