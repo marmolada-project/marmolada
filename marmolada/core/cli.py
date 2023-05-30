b8750bc9317081c4d761b7686fe40ba3a55ed832
Nils Philippsen <nils@tiptoe.de>
2023-05-31 00:03:46 +0200

    Rework handling custom configuration files
    Signed-off-by: Nils Philippsen <nils@tiptoe.de>

--- marmolada/core/cli.py
+++ marmolada/core/cli.py
@@ -1,4 +1,5 @@
 from importlib.metadata import entry_points
+from pathlib import Path
 
 import click
 import click_plugins
@@ -8,35 +9,27 @@ from . import configuration
 DEFAULT_CONFIG_FILE = "/etc/marmolada/config.yaml"
 
 
-def init_config(ctx, param, filename):
-    ctx.ensure_object(dict)
-    try:
-        configuration.read_configuration(
-            filename, clear=ctx.obj.get("clear_config", True), validate=False
-        )
-    except FileNotFoundError:
-        if filename is not DEFAULT_CONFIG_FILE:
-            raise
-    ctx.obj["clear_config"] = False
-
-
 @click_plugins.with_plugins(entry_points(group="marmolada.cli"))
 @click.group(name="marmolada")
 @click.option(
+    "config_paths",
     "-c",
     "--config",
-    type=click.Path(dir_okay=False),
-    default=DEFAULT_CONFIG_FILE,
-    callback=init_config,
-    is_eager=True,
-    expose_value=False,
-    help="Read option defaults from the specified YAML file.",
-    show_default=True,
+    type=click.Path(exists=True, dir_okay=False),
+    multiple=True,
+    help="Read option defaults from the specified YAML file(s).",
 )
 @click.option("--debug/--no-debug", default=False, help="Run with debugging enabled")
 @click.pass_context
-def cli(ctx: click.Context, debug: bool):
+def cli(ctx: click.Context, config_paths: tuple[Path], debug: bool):
     """Marmolada - a media object store."""
     ctx.ensure_object(dict)
     ctx.obj["debug"] = debug
-    configuration.read_configuration(clear=False, validate=True)
+
+    if not config_paths:
+        try:
+            configuration.read_configuration(DEFAULT_CONFIG_FILE, clear=True, validate=True)
+        except FileNotFoundError:
+            pass
+    else:
+        configuration.read_configuration(*config_paths, clear=True, validate=True)
