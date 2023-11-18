2cbe6529f1b98e611d6962b1f1b5981a32360186
Nils Philippsen <nils@tiptoe.de>
2023-11-18 13:24:03 +0100

    Make setup_db_schema() synchronous
    Signed-off-by: Nils Philippsen <nils@tiptoe.de>

--- marmolada/database/setup.py
+++ marmolada/database/setup.py
@@ -3,19 +3,19 @@ from pathlib import Path
 
 import alembic.command
 import alembic.config
-from sqlalchemy import inspect
+from sqlalchemy import create_engine, inspect
 
 from ..core.configuration import config
 
 # Import the DB model here so its classes are considered by metadata.create_all() below.
 from . import model  # noqa: F401
-from .main import get_engine, metadata
+from .main import metadata
 
 HERE = Path(__file__).parent
 
 
-async def setup_db_schema() -> None:
-    engine = get_engine()
+def setup_db_schema() -> None:
+    engine = create_engine(**config["database"]["sqlalchemy"])
 
     inspection_result = inspect(engine)
 
@@ -26,7 +26,7 @@ async def setup_db_schema() -> None:
         print("Refusing to change database schema.", file=sys.stderr)
         sys.exit(1)
 
-    async with engine.begin():
+    with engine.begin():
         print("Creating database schema")
         metadata.create_all(bind=engine)
 
