1e91625ed55d0210278fd8b2b24e4085e061eadc
Nils Philippsen <nils@tiptoe.de>
2023-12-28 00:09:32 +0100

    Add artifacts DB model
    Plus, add the necessary test infrastructure.
    
    Signed-off-by: Nils Philippsen <nils@tiptoe.de>

---
+++ marmolada/database/model/artifact.py
@@ -0,0 +1,103 @@
+import errno
+import logging
+import os
+import pathlib
+from collections import defaultdict
+from typing import Any, ClassVar
+
+from sqlalchemy import Column, Text, event
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import Session, object_session
+from sqlalchemy.sql import SQLColumnExpression
+
+from ...core.configuration import config
+from .. import Base
+from ..mixins import Creatable, Updatable, UuidPrimaryKey
+from ..types import Path
+
+log = logging.getLogger(__name__)
+
+
+class Artifact(Base, UuidPrimaryKey, Creatable, Updatable):
+    __tablename__ = "artifacts"
+
+    _sessions_added_files: ClassVar = defaultdict(set)
+    _sessions_removed_files: ClassVar = defaultdict(set)
+
+    content_type = Column(Text, nullable=False)
+    # Default for _path set in artifact_path_init() below
+    _path = Column("path", Path, unique=True, nullable=False)
+
+    artifacts_root: ClassVar[pathlib.Path | None] = None
+
+    def __new__(cls, *args: tuple, **kwargs: dict[str, Any]) -> "Artifact":
+        if not Artifact.artifacts_root:
+            Artifact.artifacts_root = pathlib.Path(config["artifacts"]["root"])
+        return super().__new__(cls)
+
+    @hybrid_property
+    def path(self) -> pathlib.PurePath:
+        return pathlib.PurePath(self._path)
+
+    @path.setter
+    def path(self, value: pathlib.PurePath | str) -> None:
+        if isinstance(value, str):
+            value = value.rstrip("/")
+        if value != self._path and self.full_path.exists():
+            new_full_path = self.artifacts_root / value
+            os.makedirs(new_full_path.parent, exist_ok=True)
+            new_full_path.hardlink_to(self.full_path)
+            self._sessions_added_files[object_session(self)].add(new_full_path)
+            self._sessions_removed_files[object_session(self)].add(self.full_path)
+
+        self._path = value
+
+    @path.expression
+    def path(cls) -> SQLColumnExpression:
+        return cls._path
+
+    @property
+    def full_path(self) -> pathlib.Path:
+        return self.artifacts_root / self.path
+
+    @hybrid_property
+    def data(self) -> bytes:
+        if self.full_path in self._sessions_removed_files[object_session(self)]:
+            raise FileNotFoundError(errno.ENOENT, "No such file or directory")
+
+        with self.full_path.open("rb") as fp:
+            return fp.read()
+
+    @data.setter
+    def data(self, data: bytes) -> None:
+        os.makedirs(self.full_path.parent, exist_ok=True)
+
+        with os.fdopen(os.open(self.full_path, os.O_CREAT | os.O_EXCL | os.O_WRONLY), "wb") as fp:
+            fp.write(data)
+
+        self._sessions_added_files[object_session(self)].add(self.full_path)
+
+    @data.deleter
+    def data(self) -> None:
+        self._sessions_removed_files[object_session(self)].add(self.full_path)
+
+
+@event.listens_for(Artifact, "init")
+def artifact_path_init(target: Artifact, args: tuple[Any], kwargs: dict[str, Any]) -> None:
+    kwargs.setdefault("_path", f"incoming/{kwargs['uuid']!s}")
+
+
+@event.listens_for(Session, "after_commit")
+def _finalize_files_on_commit(session) -> None:
+    for removed_path in Artifact._sessions_removed_files[session]:
+        removed_path.unlink()
+    del Artifact._sessions_removed_files[session]
+    Artifact._sessions_added_files.pop(session, None)
+
+
+@event.listens_for(Session, "after_soft_rollback")
+def _finalize_files_on_rollback(session, previous_transaction) -> None:
+    for added_path in Artifact._sessions_added_files[session]:
+        added_path.unlink()
+    del Artifact._sessions_added_files[session]
+    Artifact._sessions_removed_files.pop(session, None)
---
+++ tests/database/model/test_artifact.py
@@ -0,0 +1,115 @@
+from pathlib import Path
+from unittest import mock
+
+import pytest
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from marmolada.core.configuration import config
+from marmolada.database.model import Artifact
+
+from .common import ModelTestBase
+
+
+@pytest.fixture(autouse=True)
+def artifacts_root_unset_cache():
+    with mock.patch.object(Artifact, "artifacts_root", Path(config["artifacts"]["root"])):
+        yield
+
+
+@pytest.mark.marmolada_config({"artifacts": {"root": "doesn't matter"}})
+class TestArtifact(ModelTestBase):
+    cls = Artifact
+    attrs = {"content_type": "image/jpeg"}
+
+    async def test_artifacts_root(self, db_session):
+        Artifact.artifacts_root = None
+        obj = Artifact()
+        assert str(obj.artifacts_root) == config["artifacts"]["root"]
+
+    async def test_path_getter(self, db_obj: Artifact):
+        assert db_obj._path == str(db_obj.path)
+        assert db_obj._path == f"incoming/{db_obj.uuid!s}"
+
+    @pytest.mark.parametrize("test_type", (str, Path))
+    async def test_path_setter(self, test_type: type, db_obj: Artifact, db_session: Session):
+        old_path = db_obj.path
+        db_obj.path = test_type(f"{old_path}/")
+        await db_session.flush()
+        assert db_obj.path == old_path
+
+    async def test_path_expression(self, db_obj: Artifact, db_session: Session):
+        selected_obj = (
+            await db_session.execute(select(Artifact).filter_by(path=db_obj.path))
+        ).scalar_one()
+
+        assert selected_obj == db_obj
+
+    async def test_full_path(self, db_obj: Artifact):
+        assert Path(config["artifacts"]["root"]) / db_obj.path == db_obj.full_path
+
+    async def test_rename(self, db_obj: Artifact, db_session: Session):
+        async with db_session.begin_nested():
+            prev_path = db_obj.full_path
+            db_obj.data = b"Foo"
+
+        async with db_session.begin_nested():
+            db_obj.path = "new/path"
+            assert prev_path.exists()
+            assert db_obj.full_path.exists()
+            assert db_obj.data == b"Foo"
+
+        async with db_session.begin_nested():
+            assert not prev_path.exists()
+            assert db_obj.full_path.exists()
+            assert db_obj.data == b"Foo"
+            await db_session.commit()
+            assert db_obj.full_path.exists()
+            assert not prev_path.exists()
+
+    @pytest.mark.parametrize(
+        "testcase",
+        (
+            "normal",
+            "dir-doesnt-exist",
+            "dir-not-writable",
+            "file-doesnt-exist",
+            "delete",
+            "rewrite-fails",
+            "rollback",
+        ),
+    )
+    async def test_data_descriptor(self, testcase: str, db_obj: Artifact, db_session: Session):
+        artifacts_root = Path(config["artifacts"]["root"])
+
+        if testcase == "dir-not-writable":
+            artifacts_root_mode = artifacts_root.stat().st_mode
+            artifacts_root.chmod(0)
+        elif testcase == "dir-doesnt-exist":
+            artifacts_root.rmdir()
+
+        if testcase == "dir-not-writable":
+            with pytest.raises(OSError):
+                db_obj.data = b"Foo"
+            with pytest.raises(IOError):
+                db_obj.data  # noqa: B018
+            artifacts_root.chmod(artifacts_root_mode)
+        elif testcase == "file-doesnt-exist":
+            with pytest.raises(FileNotFoundError):
+                db_obj.data  # noqa: B018
+        else:  # testcase in ("normal", "dir-doesnt-exist", "delete", "rewrite-fails", "rollback")
+            db_obj.data = b"Foo"
+            assert db_obj.data == b"Foo"
+
+        if testcase == "delete":
+            del db_obj.data
+            with pytest.raises(FileNotFoundError):
+                db_obj.data  # noqa: B018
+            await db_session.commit()
+            assert not db_obj.full_path.exists()
+        elif testcase == "rewrite-fails":
+            with pytest.raises(FileExistsError):
+                db_obj.data = b"Bar"
+        elif testcase == "rollback":
+            await db_session.rollback()
+            assert not db_obj.full_path.exists()
