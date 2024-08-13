f1c62cc768f9552315e0cd01de6a42d9500bf048
Nils Philippsen <nils@tiptoe.de>
2024-08-13 10:50:35 +0200

    api: Pass configured host and port to uvicorn
    Related: #250
    
    Signed-off-by: Nils Philippsen <nils@tiptoe.de>

--- marmolada/api/cli.py
+++ marmolada/api/cli.py
@@ -1,6 +1,8 @@
 import click
 import uvicorn
 
+from ..core.configuration import config
+
 
 @click.group()
 def api() -> None:
@@ -10,4 +12,4 @@ def api() -> None:
 @api.command()
 def serve() -> None:
     """Serve the web API."""
-    uvicorn.run("marmolada.api.main:app")
+    uvicorn.run("marmolada.api.main:app", host=config["api"]["host"], port=config["api"]["port"])
--- tests/api/test_cli.py
+++ tests/api/test_cli.py
@@ -1,23 +1,14 @@
 from unittest import mock
 
-from marmolada.api.cli import api
-from marmolada.core import cli
-
-
-@api.command("test")
-def _test_api():
-    pass
+from marmolada.api import cli
 
 
 class TestCLI:
-    def test_api(self, cli_runner):
-        result = cli_runner.invoke(cli.cli, ["api", "test"])
-        assert result.exit_code == 0
-
+    @mock.patch.dict(cli.config, values={"api": {"host": "HOST", "port": 1234}}, clear=True)
     @mock.patch("marmolada.api.cli.uvicorn")
-    def test_serve(self, uvicorn, cli_runner, marmolada_config_files):
-        cli_args = [f"--config={cfpath!s}" for cfpath in marmolada_config_files]
-        cli_args.extend(["api", "serve"])
-        result = cli_runner.invoke(cli.cli, cli_args)
+    def test_serve(self, uvicorn, cli_runner):
+        result = cli_runner.invoke(cli.api, "serve")
         assert result.exit_code == 0
-        uvicorn.run.assert_called_once()
+        uvicorn.run.assert_called_once_with(
+            "marmolada.api.main:app", host=cli.config["api"]["host"], port=cli.config["api"]["port"]
+        )
