63d369f146be3970f27aa278de324a87792b84a5
Nils Philippsen <nils@tiptoe.de>
2024-11-09 12:51:08 +0100

    Fix index naming convention
    Signed-off-by: Nils Philippsen <nils@tiptoe.de>

--- marmolada/database/main.py
+++ marmolada/database/main.py
@@ -14,7 +14,7 @@ from . import types
 
 # use custom metadata to specify naming convention
 naming_convention = {
-    "ix": "%(table_name)s_%(column_0_N_labels)s_index",
+    "ix": "%(table_name)s_%(column_0_N_label)s_index",
     "uq": "%(table_name)s_%(column_0_N_name)s_key",
     "ck": "%(table_name)s_%(constraint_name)s_check",
     "fk": "%(table_name)s_%(column_0_N_name)s_%(referred_table_name)s_fkey",
